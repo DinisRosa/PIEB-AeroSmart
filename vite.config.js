363e8918e7fed8d1fba1a8fb238e46d6cf2f71d1
DinisRosa22 <dinisrosa22@gmail.com>
2026-05-27 22:42:35 +0100

    innit pwa

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'AeroSmart App',
+        short_name: 'AeroSmart',
+        theme_color: '#2563eb',
+        background_color: '#0e0e14',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'logo.png',
+            sizes: '192x192 512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ],
 })
 
